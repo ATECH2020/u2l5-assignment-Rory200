71f0c823a92dbd6eb86f730a3367cf73962dc2e5
Aurora Hart <aurora.239576@nv.ccsd.net>
2020-10-03 15:53:12 +0000

    Started it

--- src/main/java/ConstructionTester.java
+++ src/main/java/ConstructionTester.java
@@ -4,6 +4,29 @@ public class ConstructionTester
 {
     public static void main(String[] args)
     {
-        // Start here!
+        //initializes variables
+
+        double taxRate;
+        int windows;
+        int lumber;
+        double total;
+        
+        //creates a new scanner 
+        Scanner in = new Scanner(System.in);
+        
+        //asks for the values
+        System.out.println("Enter the sales tax rate:");
+        taxRate = in.nextDouble();
+        
+        System.out.println("How many boards do you need?");
+        lumber = in.nextInt();
+
+        System.out.println("How many windows do you need?");
+        windows = in.nextInt();
+
+        System.out.println("Total : ");
+
+        System.out.println("Grand Totoal : ");
+
     }
 }
--- src/main/java/HowFarAway.java
+++ src/main/java/HowFarAway.java
@@ -4,6 +4,32 @@ public class HowFarAway
 {
     public static void main(String[] args)
     {
-        // your code here.
+      //creats variables
+       double startLatitude;
+       double startLongitude;
+       double endLatitude;
+       double endLongitude;
+       double distance;
+
+       distance = distanceFrom();
+
+       GeoLocation();
+
+       Scanner scan = new Scanner(System.in);
+
+       System.out.print("Enter the latitude of the starting location:" );
+       startLatitude = scan.nextDouble();
+
+       System.out.print("Enter the longitude of the starting location: ");
+       startLongitude = scan.nextDouble();
+
+       System.out.print("Enter the latitude of the ending location: ");
+       endLatitude = scan.nextDouble();
+
+       System.out.print("Enter the longitude of the ending location: ");
+       endLongitude = scan.nextDouble();
+
+       System.out.print("The distance is " + distance + "miles");
+
     }
 }
